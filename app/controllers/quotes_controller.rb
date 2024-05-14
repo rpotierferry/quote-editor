880f20bbb7a57a300ea21b693ace19033adfbe32
Romain Potier-Ferry <romainpotierferry@gmail.com>
2024-05-14 12:58:06 +0200

    add turbo frames for crud operations

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,7 +1,7 @@
 class QuotesController < ApplicationController
   before_action :set_quote, only: [:show, :edit, :update, :destroy]
   def index
-    @quotes = Quote.all
+    @quotes = Quote.ordered
   end
 
   def show
@@ -14,26 +14,36 @@ class QuotesController < ApplicationController
   def create
     @quote = Quote.new(quote_params)
     if @quote.save
-      redirect_to quote_path(@quote), notice: "Quote successfully created"
+      respond_to do |format|
+        format.html { redirect_to quotes_path, notice: "Quote successfully created" }
+        format.turbo_stream
+      end
     else
-      render :new, :unprocessable_entity
+      render :new, status: :unprocessable_entity
     end
   end
 
   def edit
+    @quote = Quote.find(params[:id])
   end
 
   def update
     if @quote.update(quote_params)
-      redirect_to quote_path(@quote), notice: "Quote successfully updated"
+      respond_to do |format|
+        format.html { redirect_to quote_path(@quote), notice: "Quote successfully updated" }
+        format.turbo_stream { render turbo_stream: turbo_stream.replace(@quote, partial: "quotes/quote", locals: { quote: @quote }) }
+      end
     else
-      render :edit, :unprocessable_entity
+      render :edit, status: :unprocessable_entity
     end
   end
 
   def destroy
     @quote.destroy
-    redirect_to quotes_path, notice: "Quote was successfully destroyed."
+    respond_to do |format|
+      format.html { redirect_to quotes_path, notice: "Quote was successfully destroyed." }
+      format.turbo_stream { render turbo_stream: turbo_stream.remove("quote_#{params[:id]}") }
+    end
   end
 
   private
